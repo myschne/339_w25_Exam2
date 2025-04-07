df1cc2708d2e80bf10912965938facfd65480938
Jackie Wolf <jgjwolf@umich.edu>
2025-04-07 14:49:38 -0400

    Add files via upload

---
+++ js/exam2.js
@@ -0,0 +1,55 @@
+/*
+  Do NOT uncomment the example lines of code below (rows 8-10), they will cause errors.  
+  But I am providing it as a template.
+  */
+
+
+/* 
+document.querySelector("ELEMENT").addEventListener("EVENT",function(){
+     //YOUR CODE
+ })
+ */
+
+
+
+// Problem 1
+
+
+
+// Problem 2
+
+
+
+
+// Problem 3
+
+
+
+// Problem 4
+// Part 1
+
+
+// Problem 4
+// Part 2
+
+
+
+// Problem 5
+
+
+
+// Problem 6
+
+
+
+// Problem 7
+
+
+
+
+
+// Problem 8
+
+
+
+// Problem 9
